fb4879f78baed29b6d9dc8e9528ed756cf88c066
Proire <prasadahire4501@gmail.com>
2024-04-18 11:50:26 +0530

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("What is your Name ? ");
+        var name = Console.ReadLine();
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:h:mm:ss tt}");
+        Console.Write($"{Environment.NewLine}Press any key to exit...");
+        Console.ReadKey(true);
+    }
+}
