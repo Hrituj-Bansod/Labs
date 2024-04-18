d53d7222a767560a0379f2811c964153c6048ded
Proire <prasadahire4501@gmail.com>
2024-04-18 12:34:37 +0530

    Usage of Password Generator in project

---
+++ Day02/Program.cs
@@ -0,0 +1,11 @@
+﻿using PasswordGenerator;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var pwd = new Password();
+        var password = pwd.Next();
+        Console.WriteLine("Generated password : " + password.ToString());
+    }
+}
