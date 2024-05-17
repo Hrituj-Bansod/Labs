459ec0375e8ddb6fb5cd4bff2227cbf19bfc3dad
Proire <prasadahire4501@gmail.com>
2024-05-17 15:33:46 +0530

    [Prasad] Add : Mutex Code Impemented for Out-Process Threads Synchronization

--- Synchronization/Program.cs
+++ Synchronization/Program.cs
@@ -2,7 +2,16 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Application Is Running.......");
-        Console.ReadKey();
-    }
+        using (Mutex mutex = new Mutex(false, "MutexDemo"))
+        {
+            //Checking if Other External Thread is Running
+            if (!mutex.WaitOne(5000, false))
+            {
+                Console.WriteLine("An Instance of the Application is Already Running");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Application Is Running.......");
+            Console.ReadKey();
+        }
 }
