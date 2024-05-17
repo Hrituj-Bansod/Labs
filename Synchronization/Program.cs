9a406d5b62cb38155b2792de42b7ed05603bcaa0
Proire <prasadahire4501@gmail.com>
2024-05-17 17:33:54 +0530

    [Prasad] Feat : Added five threads on single resource and then implemented mutex for synchronization

--- Synchronization/Program.cs
+++ Synchronization/Program.cs
@@ -1,18 +1,37 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    private static Mutex mutex = new Mutex();
+    static void Main(string[] args)
     {
-        using (Mutex mutex = new Mutex(false, "MutexDemo"))
+        //Create multiple threads to understand Mutex
+        for (int i = 1; i <= 5; i++)
         {
-            //Checking if Other External Thread is Running
-            if (!mutex.WaitOne(5000, false))
+            Thread threadObject = new Thread(MutexDemo)
             {
-                Console.WriteLine("An Instance of the Application is Already Running");
-                Console.ReadKey();
-                return;
-            }
-            Console.WriteLine("Application Is Running.......");
-            Console.ReadKey();
+                Name = "Thread " + i
+            };
+            threadObject.Start();
+        }
+        Console.ReadKey();
+    }
+    //Method to implement syncronization using Mutex  
+    static void MutexDemo()
+    {
+        Console.WriteLine(Thread.CurrentThread.Name + " Wants to Enter Critical Section for processing");
+        try
+        {
+            //Blocks the current thread until the current WaitOne method receives a signal.  
+            //Wait until it is safe to enter. 
+            mutex.WaitOne();
+            Console.WriteLine("Success: " + Thread.CurrentThread.Name + " is Processing now");
+            Thread.Sleep(2000);
+            Console.WriteLine("Exit: " + Thread.CurrentThread.Name + " is Completed its task");
+        }
+        finally
+        {
+            //Call the ReleaseMutex method to unblock so that other threads
+            //that are trying to gain ownership of the mutex can enter  
+            mutex.ReleaseMutex();
         }
     }
 }
