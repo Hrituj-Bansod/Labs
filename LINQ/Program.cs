677661eadc41fbe4af91b94d028022676322799f
Proire <prasadahire4501@gmail.com>
2024-05-18 11:00:14 +0530

    [Prasad] Add :  LINQ Started Implementing

---
+++ LINQ/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--------------------------------------------- LINQ(Language Integrated Query) -------------------------------------------");
+
+       
+    }
+}
