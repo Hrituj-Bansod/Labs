ed59f47d0cd3d382e71176cf03fc3d6be357ba5e
Proire <prasadahire4501@gmail.com>
2024-05-18 11:40:27 +0530

    [Prasad] Feat - Basic first program implemented

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("--------------------------------------------- LINQ(Language Integrated Query) -------------------------------------------");
+        Console.WriteLine("--------------------------------------------- LINQ(Language Integrated Query) -------------------------------------------/n");
 
         // specify the data source 
         int[] scores = [93, 85, 95, 59, 78];
@@ -15,5 +15,9 @@
         {
             Console.Write(i + " ");
         }
+
+        Console.WriteLine("/n Press any key to see LINQ querying of your data source /n");
+        Console.ReadLine();
+        Console.WriteLine("/n--------------------------------------------- LINQ(Language Integrated Query) - LINQ to Objects using IEnumerable<T> -------------------------------------------/n");
     }
 }
