f2faa16187d0eb0f86a2816d6a4363777de19566
Proire <prasadahire4501@gmail.com>
2024-05-18 11:03:43 +0530

    [Prasad] Feat : Added Basic LINQ Query program feature

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,6 +4,16 @@
     {
         Console.WriteLine("--------------------------------------------- LINQ(Language Integrated Query) -------------------------------------------");
 
-       
+        // specify the data source 
+        int[] scores = [93, 85, 95, 59, 78];
+
+        // Define the query expression
+        IEnumerable<int> scoreQuery = from score in scores where score > 80 select score;
+
+        // Execute the Query
+        foreach (var i in scoreQuery)
+        {
+            Console.Write(i + " ");
+        }
     }
 }
