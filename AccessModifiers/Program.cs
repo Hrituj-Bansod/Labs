13a70015862000b1f51a1df31ed94200e26970ef
Proire <prasadahire4501@gmail.com>
2024-05-20 14:22:43 +0530

    [Prasad] Add : User Defined Library Implemented and Used

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,12 @@
+﻿using MyLibrary;
+namespace AccessModifiers
+{
+    internal class Program : PrintDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------- Using Library MyLibrary and access Methods ---------------\n");
+            Greet("Prasad");
+        }
+    }
+}
---
+++ MyLibrary/PrintDemo.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLibrary
+{
+    public class PrintDemo
+    {
+        protected static void Greet(string name)
+        {
+            Console.WriteLine("Hi "+ name);
+        }
+    }
+}
