fdd035f455a015b2d3c0437ff14dd736ba9915d6
Proire <prasadahire4501@gmail.com>
2024-05-20 15:54:47 +0530

    [Prasad] Add : Class Library Mymath and Console Application access Modifier

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -1,12 +1,37 @@
 ﻿using MyLibrary;
 namespace AccessModifiers
 {
-    internal class Program : PrintDemo
+    internal class Program : MyMathSupport
     {
+        public void Square(int x)
+        {
+            // only non-static method accesses the non-static inherited members
+            Console.WriteLine("Inside square non-static method calling square method of parent");
+            Square(x);
+        }
+
+        public void Cube(int x)
+        {
+            // only non-static method accesses the non-static inherited members
+            Console.WriteLine("Inside Cube non-static method calling square method of parent");
+            Cube(x);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("--------------- Using Library MyLibrary and access Methods ---------------\n");
-            Greet("Prasad");
+            // directly using inherited member
+            Program program = new Program();
+            program.Square(10);
+
+            // method used by creating refernce 
+            MyMathSupport myMathSupport = new MyMathSupport();
+            myMathSupport.Square(10);
+
+            //myMathSupport.cube(10);   not accessible using reference
+            program.Cube(10);
+
+            // static method sum can be called here directly
+            Sum(10, 30, 40);
         }
     }
 }
---
+++ MyLibrary/MyMathLibrary.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyLibrary
+{
+    public class MyMathLibrary
+    {
+        // Accessible anywhere inside this assembly or project(namespace) or outside the assembly
+        public void Square(int x)
+        {
+            Print(x * x);
+        }
+
+        // Internal cannot be accessed outside only used for that reason
+        internal protected void Cube(int x)
+        {
+            Cube(x * x * x);
+        }
+
+        // Accessible in same assembly in every class and outside in derived class 
+        protected internal static void Sum(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            Print(sum);
+        }
+
+        // Accessible to ony derived classes in current and also outer assemblies
+        protected void Half(int x)
+        {
+            Print(x / 2);
+        }
+
+        // Accessble only here inside this class
+        private static void Print<T>(T parameter)
+        {
+            Console.WriteLine(parameter);
+        }
+    }
+
+    public class MyMathSupport : MyMathLibrary
+    {
+        // Accessible only inside this assembly in every class not outside any class and not also in derived class outside this assembly
+        internal void Cube(int x, int y, int z)
+        {
+            Console.WriteLine(x * y * z);
+        }
+    }
+
+}
